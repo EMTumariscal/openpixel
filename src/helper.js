4ccd0acad7443c240d61d1d60f0483a146844f0b
Ulises Mariscal <umariscal@emt.dev>
2022-06-13 15:02:32 -0600

    feat: save sale from keyword

--- src/helper.js
+++ src/helper.js
@@ -20,18 +20,65 @@ class Helper {
 
       // get b value from cookies
       if (Storage.exists('b') && Storage.exists('a') && Storage.exists('e')) {
+        const akw = Storage.exists('ak') ? Storage.get('ak') : '';
+        const au = Storage.exists('au') ? Storage.get('au') : '';
+        let sale = ''
+        if (akw !==''){
+          const rakw= JSON.parse(Helper.recoveryString(akw));
+          if (typeof rakw.length === 'number' && rakw.length > 0){
+            const url = window.location.href
+            for (let i = 0; i < rakw.length; i++) {
+              const k = rakw[i];
+              const keyword = k.keyword
+              if (url.includes(keyword)) {
+                var id = k.id ? k.id : '';
+                var klass = k["class"] ? k["class"] : '';
+                var sales = [];
+
+                if (id !== '') {
+                  const getId = document.getElementById(id);
+
+                  if (getId){
+                    const html = getId.innerHTML
+                      .replace(/<[^<>]*>/g,'')
+
+                    sales.push(html)
+                  } else {
+                    console.log('soy nulo')
+                  }
+                }
+
+                if (sales.length === 0) {
+                  const klasses = document.getElementsByClassName(klass)
+                  
+                  if (klasses.length > 0) {
+                    for (let j = 0; j < klasses.length; j++) {
+                      const c = klasses[j];
+                      const html = c.innerHTML
+                        .replace(/<[^<>]*>/g,'')
+                        sales.push(html)
+                    }
+                  }
+                }
+
+                sale = sales.toString().replace(/\\n/g,'')
+              }
+            }
+          }
+        }
         return Helper.optionalData({
           "a":Storage.get('a'),
           "b":Storage.get('b'),
           "c":Storage.get('c'),
           "e":Storage.get('e'),
           "cu": Storage.exists('cu') ? Storage.get('cu') : '',
-          "au": Storage.exists('au') ? Storage.get('au') : '',
-          "ak": Storage.exists('ak') ? Storage.get('ak') : '',
+          "au": au,
+          "ak": akw,
           "lu": Storage.exists('lu') ? Storage.get('lu') : '',
           "lk": Storage.exists('lk') ? Storage.get('lk') : '',
           "banner": Storage.exists('banner') ? Storage.get('banner') : '',
-          "session": Storage.existsS('session') ? Storage.getS('session') : ''
+          "session": Storage.existsS('session') ? Storage.getS('session') : '',
+          "sale": sale
         });
       } else {
         return '';
@@ -64,4 +111,18 @@ class Helper {
   static gsuid() {
     return (1 * new Date()).toString(36);
   }
+
+  static recoveryString = (str) => {
+    return str
+      .replace(/_comma_/g, ',')
+      .replace(/_dash_/g, '/')
+      .replace(/_point_/g, '.')
+      .replace(/_colon_/g, ':')
+      .replace(/_bracketo_/g, '[')
+      .replace(/_bracketc_/g, ']')
+      .replace(/_braceo_/g, '{')
+      .replace(/_bracec_/g, '}')
+      .replace(/_quote_/g, '"')
+      .replace(/_bslash_/g, '\\');
+  };
 }
